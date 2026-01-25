bbc25edc31130ef03d3c8dc1d865c77c9e780dd5
Adridinan Najmi Faza <104964855+najmifaza@users.noreply.github.com>
2026-01-25 17:52:56 +0700

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -7,32 +7,58 @@ function NotFoundPage() {
         This example requires updating your template:
 
         ```
-        <html class="h-full">
-        <body class="h-full">
+        <html className="h-full">
+        <body className="h-full">
         ```
       */}
-      <main className=" place-items-center   sm:py-32 lg:px-8">
-        <div className="text-center">
-          <h3 className="text-base font-semibold text-indigo-400">404</h3>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-            Page not found
+      {/* <div className="max-w-3xl flex flex-col mx-auto size-full"> */}
+
+      <main id="content">
+        <div className="text-center mt-25">
+          <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">
+            404
           </h1>
-          <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
+          <p className="mt-3 text-gray-600 dark:text-neutral-400">
+            Oops, something went wrong.
           </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              <Link to="/">Go Back Home</Link>
-            </a>
-            <a href="#" className="text-sm font-semibold text-white">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+          <p className="text-gray-600 dark:text-neutral-400">
+            Sorry, we couldn't find your page.
+          </p>
+          <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+            <Link to="/">
+              {" "}
+              <a
+                className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                href="../examples.html"
+              >
+                <svg
+                  className="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m15 18-6-6 6-6" />
+                </svg>
+                Back to examples
+              </a>
+            </Link>
           </div>
         </div>
       </main>
+      <footer className="mt-auto text-center py-5">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+          <p className="text-sm text-gray-500 dark:text-neutral-500">
+            © All Rights Reserved. 2022.
+          </p>
+        </div>
+      </footer>
+      {/* </div> */}
     </>
   );
 }
